6128e80365178273fd831d20bcec4f49562ea1c1
uduma <chukwudiumah@gmail.com>
2017-03-12 12:06:54 +0100

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+// helloworld  in c
+// helloworld.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+// output the string hello world ! to the console
+printf("hello World !");
+
+  return 0;
+}
